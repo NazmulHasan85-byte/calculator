5de1c86cf2076642347dad4c0896cb88c8669d7c
NazmulHasan85-byte <nazmulhasan017191>
2021-05-23 15:18:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const firstNumber = document.getElementById('temp').value;
+const secondNumber = document.getElementById('temp2').value;
+const handleClick = ()=>{
+//    document.getElementById('numb').innerHTML= firstNumber+secondNumber 
+console.log(   document.getElementById('numb').innerHTML= firstNumber+secondNumber 
+);
+}
+ 
+    
